529ff240d1b12664775fb491bcac4ec88189772d
seaniaborbor <tarnueatalx@gmail.com>
2025-07-31 02:30:07 +0000

    ready to announce

--- app/Config/Email.php
+++ app/Config/Email.php
@@ -38,7 +38,7 @@ class Email extends BaseConfig
     /**
      * SMTP Password
      */
-    public string $SMTPPass = 'jjyymzurlvejqscz';
+    public string $SMTPPass = '';
 
     /**
      * SMTP Port
